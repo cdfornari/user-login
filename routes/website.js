2f406b550509ea2990bbac12d3c72a3b0335a850
Carlos Fornari <lilfornah@gmail.com>
2021-09-14 20:12:52 -0400

    fix directorio

--- routes/website.js
+++ routes/website.js
@@ -2,20 +2,36 @@ const { Router } = require('express');
 
 const router = Router();
 
+//router.get('/login', (req,res)=>{
+//    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/login.html');
+//})
+//router.get('/signin', (req,res)=>{
+//   res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/signin.html');
+//})
+//router.get('/restore', (req,res)=>{
+//    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/restore.html');
+//})
+//router.get('/home', (req,res)=>{
+//    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/home.html');
+//})
+//router.get('*', (req, res) => {
+//    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/404.html');
+//})
+
 router.get('/login', (req,res)=>{
-    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/login.html');
+    res.sendFile(__dirname + '/public/login.html');
 })
 router.get('/signin', (req,res)=>{
-    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/signin.html');
+    res.sendFile(__dirname + '/public/signin.html');
 })
 router.get('/restore', (req,res)=>{
-    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/restore.html');
+    res.sendFile(__dirname + '/public/restore.html');
 })
 router.get('/home', (req,res)=>{
-    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/home.html');
+    res.sendFile(__dirname + '/public/home.html');
 })
 router.get('*', (req, res) => {
-    res.sendFile(__dirname.substring(0, __dirname.indexOf("\\routes")) + '/public/404.html');
+    res.sendFile(__dirname + '/public/404.html');
 })
 
 module.exports = router;
